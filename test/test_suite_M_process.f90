239db8bec2d8586771169b68b0c012a1de043dc2
John S. Urban <urbanjost@comcast.net>
2023-04-11 22:44:46 -0400

    update for fpm 0.8.0

--- test/test_suite_M_process.f90
+++ test/test_suite_M_process.f90
@@ -3,7 +3,8 @@ use M_msg
 use :: M_verify,  only : unit_check_command, unit_check_keep_going, unit_check_level
 use :: M_verify,  only : unit_check_stop
 implicit none
-external test_suite_M_process
+!external test_suite_M_process
+interface; subroutine test_suite_M_process(); end ; end interface
    unit_check_command=''
    unit_check_keep_going=.true.
    unit_check_level=0
